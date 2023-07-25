893482bbcf1f5515cfb5d589b139a671f0e72dc1
Dizelars <ars_logachev@mail.ru>
2023-07-25 21:38:00 +0300

    style

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,144 @@ const devMode = mode === 'development';
 const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        open: true,
+    },
+    entry: {
+        index: './src/index.js',
+        'index-style': './src/style/main_page.css',
+        '404': './src/js/pages/404.js',
+        '404-style': './src/style/pages/404.css',
+        vectary: './src/js/pages/vectary.js',
+        // 'vectary-style': './src/style/pages/vectary.css',
+        'vectary-style': './src/style/style.css',
+        amarok: './src/js/pages/amarok.js',
+        'amarok-style': './src/style/style.css',
+        solaris: './src/js/pages/solaris.js',
+        // 'solaris-style': './src/style/pages/solaris.css',
+        'solaris-style': './src/style/style.css',
+        solaris_two: './src/js/pages/solaris_two.js',
+        'solaris_two-style': './src/style/style.css',
+        bus: './src/js/pages/bus.js',
+        'bus-style': './src/style/style.css',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'javascript/[name].[contenthash].js',
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                styles: {
+                    name: 'styles',
+                    type: 'css/mini-extract',
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+            chunks: ['index', 'index-style'],
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/pages', '404.html'),
+            filename: 'pages/404.html',
+            chunks: ['404', '404-style'],
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/pages', 'vectary.html'),
+            filename: 'pages/vectary.html',
+            chunks: ['vectary', 'vectary-style'],
+            // chunks: ['vectary'],
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/pages', 'amarok.html'),
+            filename: 'pages/amarok.html',
+            chunks: ['amarok', 'amarok-style'],
+            // chunks: ['amarok'],
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/pages', 'solaris.html'),
+            filename: 'pages/solaris.html',
+            chunks: ['solaris', 'solaris-style'],
+            // chunks: ['solaris'],
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/pages', 'solaris_two.html'),
+            filename: 'pages/solaris_two.html',
+            chunks: ['solaris_two', 'solaris_two-style'],
+            // chunks: ['solaris_two'],
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/pages', 'bus.html'),
+            filename: 'pages/bus.html',
+            chunks: ['bus', 'bus-style'],
+            // chunks: ['bus'],
+            minify: false,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style/[name].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            }
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.otf?$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+        ]
+    }
+}
+
 // module.exports = {
 //     mode,
 //     target,
@@ -102,140 +240,3 @@ const devtool = devMode ? 'source-map' : undefined;
 //         ]
 //     }
 // }
-
-module.exports = {
-    mode,
-    target,
-    devtool,
-    devServer: {
-        open: true,
-    },
-    entry: {
-        index: './src/index.js',
-        '404': './src/js/pages/404.js',
-        '404-style': './src/style/pages/404.css',
-        vectary: './src/js/pages/vectary.js',
-        // 'vectary-style': './src/style/pages/vectary.css',
-        'vectary-style': './src/style/style.css',
-        amarok: './src/js/pages/amarok.js',
-        'amarok-style': './src/style/style.css',
-        solaris: './src/js/pages/solaris.js',
-        // 'solaris-style': './src/style/pages/solaris.css',
-        'solaris-style': './src/style/style.css',
-        solaris_two: './src/js/pages/solaris_two.js',
-        'solaris_two-style': './src/style/style.css',
-        bus: './src/js/pages/bus.js',
-        'bus-style': './src/style/style.css',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'javascript/[name].[contenthash].js',
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                default: false,
-                styles: {
-                    name: 'styles',
-                    type: 'css/mini-extract',
-                    chunks: 'all',
-                    enforce: true,
-                },
-            },
-        },
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            filename: 'index.html',
-            chunks: ['index'],
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages', '404.html'),
-            filename: 'pages/404.html',
-            chunks: ['404', '404-style'],
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages', 'vectary.html'),
-            filename: 'pages/vectary.html',
-            chunks: ['vectary', 'vectary-style'],
-            // chunks: ['vectary'],
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages', 'amarok.html'),
-            filename: 'pages/amarok.html',
-            chunks: ['amarok', 'amarok-style'],
-            // chunks: ['amarok'],
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages', 'solaris.html'),
-            filename: 'pages/solaris.html',
-            chunks: ['solaris', 'solaris-style'],
-            // chunks: ['solaris'],
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages', 'solaris_two.html'),
-            filename: 'pages/solaris_two.html',
-            chunks: ['solaris_two', 'solaris_two-style'],
-            // chunks: ['solaris_two'],
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages', 'bus.html'),
-            filename: 'pages/bus.html',
-            chunks: ['bus', 'bus-style'],
-            // chunks: ['bus'],
-            minify: false,
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style/[name].[contenthash].css',
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [require('postcss-preset-env')],
-                            }
-                        }
-                    },
-                ],
-            },
-            {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['@babel/preset-env', { targets: "defaults" }]
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.otf?$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: 'fonts/[name][ext]'
-                }
-            },
-        ]
-    }
-}
