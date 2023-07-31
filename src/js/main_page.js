4b32f885d60710e3a8c52bdbcbb2a23234d525ff
Dizelars <ars_logachev@mail.ru>
2023-07-31 19:11:13 +0300

    adaptive + footer

--- src/js/main_page.js
+++ src/js/main_page.js
@@ -9,3 +9,74 @@ burgerMenus.forEach((burgerMenu) => {
         this.classList.toggle("burger-menu--closed");
     });
 });
+
+// Swiper slider
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.btn_next',
+        prevEl: '.btn_prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    // loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination2',
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.btn_next',
+    //     prevEl: '.btn_prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+    // Optional parameters
+    // loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination3',
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.btn_next',
+    //     prevEl: '.btn_prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
