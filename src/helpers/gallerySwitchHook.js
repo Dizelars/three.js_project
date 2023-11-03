94965fb6027c87daf3dd4936d478266295ea54b1
denisov-vo <denisov-vo@mosmetro.ru>
2023-11-03 16:25:18 +0300

    Фикс слайдера

--- src/helpers/gallerySwitchHook.js
+++ src/helpers/gallerySwitchHook.js
@@ -4,6 +4,7 @@ export class GallerySwitchHook {
     prevSelector = ".prev_button",
     nextSelector = ".next_button",
   }) {
+    this.galleryItemsAmount = 4;
     this.currentIndex = 0;
     this.container = document.querySelector(containerSelector);
     this.prevButton = this.container.querySelector(prevSelector);
@@ -20,6 +21,11 @@ export class GallerySwitchHook {
     this.slides = this.sliderWrapper.querySelectorAll(".gallery_item");
 
     window.addEventListener("load", (e) => {
+      // Здесь можно добавить проверку, в каком мы сейчас разрешении, и в соответствии с этим менять this.galleryItemsAmount
+      const widthPerSlide = this.wrapperWidth() / this.galleryItemsAmount;
+      this.slides.forEach((slide) => {
+        slide.style.width = `${widthPerSlide - parseInt(getComputedStyle(slide).marginLeft) - parseInt(getComputedStyle(slide).marginRight)}px`;
+      })
       this.checkPrevButton();
       this.checkNextButton();
     });
@@ -48,11 +54,22 @@ export class GallerySwitchHook {
       parseInt(getComputedStyle(this.slides[0]).marginLeft) +
       parseInt(getComputedStyle(this.slides[0]).marginRight);
     const translateAmount = -slideWidth * this.currentIndex - diff;
+
+    // если свайпаем левее самого левого слайда:
+    if (translateAmount > 0) {
+      return;
+    }
+
+    // если свайпаем правее самого правого слайда:
+    if (this.isItemsOver()) {
+      return;
+    }
+
     this.sliderWrapper.style.transform = `translate3d(${translateAmount}px, 0, 0)`;
   }
   handleTouchEnd() {
     const diff = this.startPosX - this.currentPosX;
-    if (diff > 0) {
+    if (diff > 0 && !this.isItemsOver()) {
       this.slideToNext();
     } else if (diff < 0) {
       this.slideToPrev();
